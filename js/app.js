435e468e67badf6254e1ec4e0d416f07d61c3908
AbdalrahmanAbualata <abuataabooood@yahoo.com>
2021-07-06 00:39:49 +0300

    lab11-day2*

--- js/app.js
+++ js/app.js
@@ -13,8 +13,8 @@ let ulEl = document.getElementById('results');
 resultsContainerEl.appendChild(ulEl );
 let productsName=[];
 let products = [];
-let votes =[];
-let views =[];
+// let votes =[];
+// let views =[];
 let attempts = 1;
 let maxAttempts = 25;
 let productsImages=['bag.jpg','banana.jpg','bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg','chair.jpg','cthulhu.jpg','dog-duck.jpg' , 'dragon.jpg', 'pen.jpg ', 'pet-sweep.jpg', 'scissors.jpg' , 'shark.jpg', 'sweep.jpg' , 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg' ]
@@ -80,14 +80,13 @@ leftImgEl.addEventListener('click', countTheVotes);
 midImgEl.addEventListener('click', countTheVotes);
 rightImgEl.addEventListener('click', countTheVotes);
 
-spanEl.addEventListener('click',resultsfun );
 
 function countTheVotes(event) {
 
 
     if (attempts < maxAttempts) {
        
-        let clickedImg = event.target.id;
+        let clickedImg = event.target.id;//any img i clicked .
         if (clickedImg === 'leftImg') {
             products[leftIndex].votes++;
         }
@@ -109,11 +108,13 @@ function countTheVotes(event) {
     }
     attempts++;
 }
-
+let votes2=[];
+let views2 =[];
 spanEl.addEventListener('click',resultsfun );
 
 function resultsfun(event){
-    
+    let votes =[];
+    let views =[];
 ulEl.textContent="";/*to remove the  content in the ul and fill it in the for loop  */
 let clickedResult= event.target.id;
 for (let i = 0; i < products.length; i++) {
@@ -122,6 +123,10 @@ for (let i = 0; i < products.length; i++) {
     ulEl.appendChild(liEl);
     votes.push( products[i].votes)
     views.push( products[i].views)
+    // if (attempts>24) {
+    //     votes2=votes;
+    //     views2=views;
+    // }
 }
 
     if (attempts > 24) {
@@ -174,58 +179,6 @@ var myChart = new Chart(ctx, {
 }
 
 }
-
-// var ctx = document.getElementById('myChart').getContext('2d');
-// var myChart = new Chart(ctx, {
-//     type: 'bar',
-//     data: {
-//         labels: productsName,
-//         datasets: [{
-//             label: '# of Votes',
-//             data: votes,
-//             backgroundColor: [
-//                 'rgba(255, 206, 86, 1)'
-                
-//             ],
-//             borderColor: [
-              
-//                 'rgba(255, 206, 86, 1)',
-    
-//             ],
-//             borderWidth: 1
-//         },
-//         {
-//             label: '# of views',
-//             data:views,
-//             backgroundColor: [
-//                 'rgba(54, 162, 235, 1)'
-                
-//             ],
-//             borderColor: [
-                
-//                 'rgba(54, 162, 235, 1)'
-               
-//             ],
-//             borderWidth: 1
-//         }]
-//     },
-//     options: {responsive: false,
-//         maintainAspectRatio: true,
-//         scales: {
-//             y: {
-//                 beginAtZero: true
-    
-//             }
-//         }
-//     }
-// });
-
-
-
-
-
-
-
 
 // let pEl=document.createElement('p');
 // pEl.addEventListener('click',resultsfun )
